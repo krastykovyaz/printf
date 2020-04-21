b114a9d19f47628ab1b6dab44b46ad24c065f6a3
Krasty <krasty_90@mail.ru>
2020-04-21 20:57:28 +0300

    add15

--- type_float_size.c
+++ type_float_size.c
@@ -1,34 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   type_float_size.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aleksandrkovazin <aleksandrkovazin@stud    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/04/21 20:42:07 by aleksandrko       #+#    #+#             */
+/*   Updated: 2020/04/21 20:44:15 by aleksandrko      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "printf.h"
 
-int len_a_dot(t_pr *stut)
-{   
-    int nb;
-    char *s;
-    int i;
+int		len_a_dot(t_pr *stut)
+{
+	int		nb;
+	char	*s;
+	int		i;
 
-    nb = stut->fl_save;
-    s = ft_itoa(nb);
-    i = ft_strlen(s) + 1;
-    free(s);
-    if (stut->dot == 0)
-        i += 6;
-    else if (stut->dot && !stut->accuracy)
-        i--;
-    else
-        i += stut->accuracy;
-    return(i);
+	nb = stut->fl_save;
+	s = ft_itoa(nb);
+	i = ft_strlen(s) + 1;
+	free(s);
+	if (stut->dot == 0)
+		i += 6;
+	else if (stut->dot && !stut->accuracy)
+		i--;
+	else
+		i += stut->accuracy;
+	return (i);
 }
 
-int type_float_size(va_list ap,t_pr *stut)
+int		type_float_size(va_list ap, t_pr *stut)
 {
-    if (stut->size == 1)
-        stut->fl = va_arg(ap, double);
-    else if (stut->size == 3)
-        stut->fl = va_arg(ap, long double);
-    else
-        stut->fl = (double)va_arg(ap, double);
-    stut->fl_save = stut->fl;
-    stut->len = len_a_dot(stut);
-    type_float_priority(stut);
-    return (0);
+	if (stut->size == 1)
+		stut->fl = va_arg(ap, double);
+	else if (stut->size == 3)
+		stut->fl = va_arg(ap, long double);
+	else
+		stut->fl = (double)va_arg(ap, double);
+	stut->fl_save = stut->fl;
+	stut->len = len_a_dot(stut);
+	type_float_priority(stut);
+	return (0);
 }
--- type_str.c
+++ type_str.c
@@ -1,99 +1,117 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   type_str.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aleksandrkovazin <aleksandrkovazin@stud    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/04/21 20:04:43 by aleksandrko       #+#    #+#             */
+/*   Updated: 2020/04/21 20:34:51 by aleksandrko      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "printf.h"
 
-int type_s_minus(t_pr *stut)
+void	minusifelse(int i, t_pr *stut)
 {
-    int i;
-    
-    i = ft_strlen(stut->s);
-    if (!stut->width || stut->accuracy > stut->width)
-        (stut->accuracy > i) ? ft_putstr(stut->s, stut) :\
-         ft_putnstr(stut->s, stut->accuracy, stut);
-    else if(i > stut->width && !stut->dot)
-        ft_putstr(stut->s, stut);
-    if (stut->width > stut->accuracy && stut->accuracy)
-    {
-        if (stut->accuracy > i || !stut->dot)
-        {
-            ft_putnstr(stut->s, i, stut);
-            ft_putnchar(' ', stut->width - i, stut);
-        }
-        else
-        {
-            ft_putnstr(stut->s, stut->accuracy, stut);
-            ft_putnchar(' ', stut->width - stut->accuracy, stut);
-        }
-    }
-    else if (stut->width && !stut->accuracy && stut->dot)
-        ft_putnchar(' ',  stut->width, stut);
-    else if (stut->width >= i)
-    {
-        ft_putstr(stut->s, stut);
-        ft_putnchar(' ', stut->width - i, stut);
-    }
-    return (0);
+	if (stut->accuracy > i || !stut->dot)
+	{
+		ft_putnstr(stut->s, i, stut);
+		ft_putnchar(' ', stut->width - i, stut);
+	}
+	else
+	{
+		ft_putnstr(stut->s, stut->accuracy, stut);
+		ft_putnchar(' ', stut->width - stut->accuracy, stut);
+	}
 }
 
-int type_s_regular(t_pr *stut)
+int		type_s_minus(t_pr *stut)
 {
-    int i;
-    
-    i = ft_strlen(stut->s);
-    if (stut->width >= stut->accuracy && stut->accuracy)
-    {
-        if (stut->accuracy > i || !stut->dot)
-        {
-                    // printf("F\n");
-            ft_putnchar(' ', stut->width - i, stut);
-            ft_putnstr(stut->s, i, stut);
-        }
-        else
-        {
-            
-            ft_putnchar(' ', stut->width - stut->accuracy, stut);
-            ft_putnstr(stut->s, stut->accuracy, stut);
-        }
-    }
-    else if (stut->width && !stut->accuracy && stut->dot)
-        ft_putnchar(' ', stut->width, stut);
-    else if (stut->width >= i)
-    {
-        ft_putnchar(' ', stut->width - i, stut);
-        ft_putstr(stut->s, stut);
-    }
-    else if ((!stut->width && stut->accuracy) || stut->accuracy > stut->width)
-        stut->accuracy > i ? ft_putstr(stut->s, stut) :\
-         ft_putnstr(stut->s, stut->accuracy, stut);
-    else if (i > stut->width && i > stut->accuracy && !stut->dot)
-        ft_putstr(stut->s, stut);
-    return (0);
+	int	i;
+
+	i = ft_strlen(stut->s);
+	if (!stut->width || stut->accuracy > stut->width)
+		(stut->accuracy > i) ? ft_putstr(stut->s, stut) :\
+	ft_putnstr(stut->s, stut->accuracy, stut);
+	else if (i > stut->width && !stut->dot)
+		ft_putstr(stut->s, stut);
+	if (stut->width > stut->accuracy && stut->accuracy)
+	{
+		minusifelse(i, stut);
+	}
+	else if (stut->width && !stut->accuracy && stut->dot)
+		ft_putnchar(' ', stut->width, stut);
+	else if (stut->width >= i)
+	{
+		ft_putstr(stut->s, stut);
+		ft_putnchar(' ', stut->width - i, stut);
+	}
+	return (0);
 }
 
-int type_str(va_list ap, const char *format, t_pr *stut)
+void	regifelse(int i, t_pr *stut)
 {
-    if (format[stut->i] == 'c')
-    {
-        stut->a = va_arg(ap, unsigned int);
-        // printf("%lli", stut->a);
-        if (stut->minus)
-        {
-            // printf("f\n");
-            ft_putchar(stut->a, stut);
-            ft_putnchar(' ', stut->width - 1, stut);
-        }
-        else{
-            if (stut->width)
-                (stut->a >= 0) ? ft_putnchar(' ', stut->width - 1, stut) : ft_putnchar(' ', stut->width, stut);
-            ft_putchar(stut->a, stut);}
-    }
-    else
-    {
-        stut->s = va_arg(ap, char *);
-        if (!stut->s)
-            stut->s = "(null)";
-        if (stut->minus)
-            type_s_minus(stut);
-        else
-            type_s_regular(stut);
-    } 
-    return (0);
+	if (stut->accuracy > i || !stut->dot)
+	{
+		ft_putnchar(' ', stut->width - i, stut);
+		ft_putnstr(stut->s, i, stut);
+	}
+	else
+	{
+		ft_putnchar(' ', stut->width - stut->accuracy, stut);
+		ft_putnstr(stut->s, stut->accuracy, stut);
+	}
+}
+
+int		type_s_regular(t_pr *stut)
+{
+	int	i;
+
+	i = ft_strlen(stut->s);
+	if (stut->width >= stut->accuracy && stut->accuracy)
+	{
+		regifelse(i, stut);
+	}
+	else if (stut->width && !stut->accuracy && stut->dot)
+		ft_putnchar(' ', stut->width, stut);
+	else if (stut->width >= i)
+	{
+		ft_putnchar(' ', stut->width - i, stut);
+		ft_putstr(stut->s, stut);
+	}
+	else if ((!stut->width && stut->accuracy) || stut->accuracy > stut->width)
+		stut->accuracy > i ? ft_putstr(stut->s, stut) :\
+	ft_putnstr(stut->s, stut->accuracy, stut);
+	else if (i > stut->width && i > stut->accuracy && !stut->dot)
+		ft_putstr(stut->s, stut);
+	return (0);
+}
+
+int		type_str(va_list ap, const char *format, t_pr *stut)
+{
+	if (format[stut->i] == 'c')
+	{
+		stut->a = va_arg(ap, unsigned int);
+		if (stut->minus)
+		{
+			ft_putchar(stut->a, stut);
+			ft_putnchar(' ', stut->width - 1, stut);
+		}
+		else
+		{
+			if (stut->width)
+				(stut->a >= 0) ? ft_putnchar(' ', stut->width - 1, stut) :\
+	ft_putnchar(' ', stut->width, stut);
+			ft_putchar(stut->a, stut);
+		}
+	}
+	else
+	{
+		stut->s = va_arg(ap, char *);
+		if (!stut->s)
+			stut->s = "(null)";
+		stut->minus ? type_s_minus(stut) : type_s_regular(stut);
+	}
+	return (0);
 }
